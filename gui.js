4c321d83af241b40803d5fe3985409977daa7aca
Joaquín E. Serraiti <joaserraiti@gmail.com>
2025-09-11 15:59:07 -0300

    Update gui.js
    - Update gui script to support the new layout and features.
    
    Refactored some parts of the code for simplicity as well.

--- gui.js
+++ gui.js
@@ -7,11 +7,21 @@ const sudokuGrid = document.getElementById("grid");
 const inputField = document.getElementById("gridDigitsInput");
 const inputButton = document.getElementById("inputButton");
 const inputAlert = document.getElementById("inputAlert");
-const analyzeButton = document.getElementById("analyzeButton");
+const visualizeAnalysisButton = document.getElementById("analyzeButton");
 const analysisAlert = document.getElementById("analysisAlert");
-const visualizeCheckBox = document.getElementById("visualizationToggle");
 const gridValidityParagraph = document.getElementById("validity");
 const gridCompletenessParagraph = document.getElementById("completeness");
+
+const transformationsList = document.getElementById("transformationsList");
+const applyTransformationButton = document.getElementById("applyTransformationButton");
+
+const showMetricsButton = document.getElementById("showMetricsButton");
+const showMetricsText = document.getElementById("showMetricsText");
+const showMetricsArrow = document.getElementById("showMetricsArrow");
+
+const patternsMetricsPanel = document.getElementById("patternsMetrics");
+
+
 
 const IBPUmetricsWrapper = document.getElementById("IBPUMetricsWrapper");
 const IBPAmetricsWrapper = document.getElementById("IBPAMetricsWrapper");
@@ -35,8 +45,8 @@ const TDCmetric1 = document.getElementById("TDCMetric1");
 const TDCmetric2 = document.getElementById("TDCMetric2");
 const DACmetric1 = document.getElementById("DACMetric1");
 
-canvas.width = sudokuGrid.offsetWidth-2;
-canvas.height = sudokuGrid.offsetHeight-2;
+canvas.width = sudokuGrid.offsetWidth-4;
+canvas.height = sudokuGrid.offsetHeight-4;
 
 for (let i1 = 0; i1 < 9; i1++) {
     const box = document.createElement("div");
@@ -57,10 +67,11 @@ for (let i1 = 0; i1 < 9; i1++) {
 }
 
 const cellWidth = document.getElementById("cell0").offsetWidth;
-const visualizationTool = new Visualize(canvas, cellWidth, 2, 2, 1);
+const visualizationTool = new Visualize(canvas, cellWidth-0.5, 2, 2, 1);
+let visualizingAnalisis = false;
+const sudokuLab = new SudokuLab(undefined,visualizationTool)
 
-let isSudokuStringProcessed = false;
-let gridData = undefined;
+
 
 function processSudokuString(sudokuString) {
 
@@ -68,48 +79,45 @@ function processSudokuString(sudokuString) {
     
     if (sudokuString.length !== 81) {
         inputField.style.borderColor = "rgba(145, 0, 0, 1)";
-        inputField.style.backgroundColor = "rgb(255, 239, 239)";
+        inputField.style.backgroundColor = "rgba(255, 227, 227, 1)";
         inputAlert.innerText = "String must have 81 characters. Current number: "+sudokuString.length;
         inputAlert.style.display = "initial"
-        return false;
+        return false
     }
 
     for (let i = 0; i < 81; i++) {
         if (charToNum[sudokuString[i]] !== 0 && !charToNum[sudokuString[i]]) {
             inputField.style.borderColor = "rgba(145, 0, 0, 1)";
-            inputField.style.backgroundColor = "rgb(255, 239, 239)";
+            inputField.style.backgroundColor = "rgba(255, 227, 227, 1)";
             inputAlert.innerText = "String must have valid characters: .0123456789";
             inputAlert.style.display = "initial"
-            return false;
+            return false
         }
     }
 
-    isSudokuStringProcessed = true
-    gridData = new SudokuLab(sudokuString, visualizationTool);
-
-    if (gridData.isGridValid) {
-        gridValidityParagraph.innerText = "Valid - No repeated digits in columns, rows or boxes."
-    } else {
-        gridValidityParagraph.innerText = "Invalid - Repeated digits in columns, rows or boxes."
-    }
-
-    if (gridData.isGridComplete) {
-        gridCompletenessParagraph.innerText = "Complete - There are no empty cells.";
-    } else {
-        gridCompletenessParagraph.innerText = "Incomplete - There are empty cells.";
-    }
+    sudokuLab.setNewGrid(sudokuString);
     return true
 }
 
-function renderGridDigits(sudokuString) {
-    processSudokuString(sudokuString);
-    if (!isSudokuStringProcessed) {
-        return
+function determineCompletenessAndValidity() {
+    let validAndComplete = true;
+    if (sudokuLab.isGridValid) {
+        gridValidityParagraph.innerText = "Valid - No repeated digits in columns, rows or boxes."
+    } else {
+        gridValidityParagraph.innerText = "Invalid - Repeated digits in columns, rows or boxes."
+        validAndComplete = false;
     }
-    inputField.value = "";
-    inputField.style.borderColor = "rgb(51, 53, 63)";
-    inputField.style.backgroundColor = "white";
-    inputAlert.style.display = "none";
+
+    if (sudokuLab.isGridComplete) {
+        gridCompletenessParagraph.innerText = "Complete - There are no empty cells.";
+    } else {
+        gridCompletenessParagraph.innerText = "Incomplete - There are empty cells.";
+        validAndComplete = false;
+    }
+    return validAndComplete
+}
+
+function displayDigits(sudokuString) {
     for (let i = 0; i < 81; i++) {
         const cellParagraph = document.getElementById("p"+i);
         let digit = ""+sudokuString[i]
@@ -118,48 +126,48 @@ function renderGridDigits(sudokuString) {
     }
 }
 
+function renderGridDigits(sudokuString) {
+    if (!processSudokuString(sudokuString)) {
+        return false
+    }
+    determineCompletenessAndValidity();
+    inputField.value = "";
+    inputField.style.borderColor = "rgb(51, 53, 63)";
+    inputField.style.backgroundColor = "white";
+    inputAlert.style.display = "none";
+    
+    displayDigits(sudokuString);
+
+    analyzePatterns();
+    fillTransformationList();
+    if (!(sudokuLab.isGridComplete && sudokuLab.isGridValid)) {
+        resetPatternMetrics();
+        clearTranformationsList();
+    }
+    return true
+}
+
 async function inputButtonPressed() {
+    if (visualizingAnalisis) {return}
     const sudokuString = inputField.value;
-    isSudokuStringProcessed = false;
-    renderGridDigits(sudokuString);
-
-    gridCompletenessParagraph.style.color = "black";
-    gridValidityParagraph.style.color = "black";
-    analysisAlert.style.display = "none";
-
-    IBPUmetricsWrapper.style.height = "0px";
-    IBPUmetricsWrapper.style.opacity = "0%";
-    IBPAmetricsWrapper.style.height = "0px";
-    IBPAmetricsWrapper.style.opacity = "0%";
-    TDCmetricsWrapper.style.height = "0px";
-    TDCmetricsWrapper.style.opacity = "0%";
-    DACmetricsWrapper.style.height = "0px";
-    DACmetricsWrapper.style.opacity = "0%";
-
-    IBPUProgressBar.style.width = `0%`;
-    IBPAProgressBar.style.width = `0%`;
-    TDCProgressBar.style.width = `0%`;
-    DACProgressBar.style.width = `0%`;
-    resetBarsColor()
-
-    IBPUpercentage.innerText = "-%";
-    IBPApercentage.innerText = "-%";
-    TDCpercentage.innerText = "-%";
-    DACpercentage.innerText = "-%";
-
-    IBPUmetric1.innerText = "-";
-    IBPAmetric1.innerText = "-";
-    IBPAmetric2.innerText = "-";
-    TDCmetric1.innerText = "-";
-    TDCmetric2.innerText = "-";
-    DACmetric1.innerText = "-";
+    if (renderGridDigits(sudokuString)) {
+        if (sudokuLab.isGridComplete) {
+            gridCompletenessParagraph.style.color = "white";
+    }
+        if (sudokuLab.isGridValid) {
+            gridValidityParagraph.style.color = "white";
+        }  
+    }
+    if (sudokuLab.isGridComplete && sudokuLab.isGridValid) {
+        analysisAlert.style.display = "none";
+    }
 }
 inputButton.onclick = inputButtonPressed;
 
 function getBarColor(percentage) {
     const number = (255/100)*percentage;
     const difference = Math.abs((2*number) - 255);
-    return `rgb(${255-number},${number},${60+255-difference})`
+    return `rgb(${265-number},${10+number},${60+255-(difference/2)})`
 }
 
 function resetBarsColor() {
@@ -169,31 +177,43 @@ function resetBarsColor() {
     DACProgressBar.style.backgroundColor = "rgb(243, 90, 105)"
 }
 
-async function analyzeButtonPressed() {
-    if (!gridData) {return}
-    if (!gridData.isGridComplete || !gridData.isGridValid) {
-        if (!gridData.isGridComplete) {
-            gridCompletenessParagraph.style.color = "rgba(230, 5, 5, 1)";
+function resetPatternMetrics() {
+    IBPUpercentage.innerText = "-%";
+    IBPApercentage.innerText = "-%";
+    TDCpercentage.innerText = "-%";
+    DACpercentage.innerText = "-%";
+
+    IBPUmetric1.innerText = "---";
+    IBPAmetric1.innerText = "---";
+    IBPAmetric2.innerText = "---";
+    TDCmetric1.innerText = "---";
+    TDCmetric2.innerText = "---";
+    DACmetric1.innerText = "---";
+
+    IBPUProgressBar.style.width = `0%`;
+    IBPAProgressBar.style.width = `0%`;
+    TDCProgressBar.style.width = `0%`;
+    DACProgressBar.style.width = `0%`;
+
+    resetBarsColor();
+}
+
+async function analyzePatterns() {
+    if (!sudokuLab.isGridComplete || !sudokuLab.isGridValid) {
+        if (!sudokuLab.isGridComplete) {
+            gridCompletenessParagraph.style.color = "rgba(255, 138, 138, 1)";
         }
-        if (!gridData.isGridValid) {
-            gridValidityParagraph.style.color = "rgba(230, 5, 5, 1)";
+        if (!sudokuLab.isGridValid) {
+            gridValidityParagraph.style.color = "rgba(255, 138, 138, 1)";
         }
         analysisAlert.style.display = "initial";
         return
     }
 
     analysisAlert.style.display = "none";
-    gridCompletenessParagraph.style.color = "black";
-    const analysisReport = await gridData.analysisReport(visualizeCheckBox.checked);
-
-    IBPUmetricsWrapper.style.height = "23px";
-    IBPUmetricsWrapper.style.opacity = "100%";
-    IBPAmetricsWrapper.style.height = "76px";
-    IBPAmetricsWrapper.style.opacity = "100%";
-    TDCmetricsWrapper.style.height = "45px";
-    TDCmetricsWrapper.style.opacity = "100%";
-    DACmetricsWrapper.style.height = "23px";
-    DACmetricsWrapper.style.opacity = "100%";
+    gridCompletenessParagraph.style.color = "white";
+    gridValidityParagraph.style.color = "white";
+    const analysisReport = await sudokuLab.analysisReport();
 
     IBPUProgressBar.style.width = `${analysisReport["IBPU"]["percentage"]}%`;
     IBPUProgressBar.style.backgroundColor = getBarColor(analysisReport["IBPU"]["percentage"]);
@@ -214,12 +234,172 @@ async function analyzeButtonPressed() {
     TDCmetric1.innerText = analysisReport["TDC"]["metrics"][0]+analysisReport["TDC"]["metrics"][1];
     TDCmetric2.innerText = analysisReport["TDC"]["metrics"][2]+analysisReport["TDC"]["metrics"][3];
     DACmetric1.innerText = analysisReport["DAC"]["metrics"][0];
-
 }
-analyzeButton.onclick = analyzeButtonPressed;
+
+function clearTranformationsList() {
+    transformationsList.replaceChildren();
+}
+
+function fillTransformationList() {
+    visualizationTool.clearCanvas();
+    clearTranformationsList();
+    sudokuLab.findTransformations();
+    const transformations = sudokuLab.transformations;
+
+    for (const transformationType of ["digitSwaps1","digitSwaps2","digitSwaps3","tripletSwaps"]) {
+        for (const transformation of transformations[transformationType]) {
+            createTransformationElement(transformationType,transformation);
+        }
+    }
+}
+
+function displayTransformation(transformationType,transformation) {
+    if (visualizingAnalisis) {return}
+    visualizationTool.clearCanvas();
+    const colorMap = {
+        "digitSwaps1":"rgba(0, 201, 167, 0.25)",
+        "digitSwaps2":"rgba(4, 78, 238, 0.25)",
+        "digitSwaps3":"rgba(105, 25, 219, 0.25)",
+        "tripletSwaps":"rgba(212, 23, 67, 0.25)"};
+    for (const cellPair of transformation) {
+        for (const cellIndex of cellPair) {
+            visualizationTool.drawCell(colorMap[transformationType],cellIndex);
+        }
+    }
+}
+
+visualizeAnalysisButton.onclick = async () => {
+    visualizationTool.clearCanvas();
+    if (visualizingAnalisis) {return}
+    visualizingAnalisis = true;
+    await sudokuLab.analysisReport(true)
+    visualizingAnalisis = false;
+}
 
 
 
+showMetricsButton.onclick = showMetricsButtonPressed;
+let showing = false
+function showOrHideMetrics(show) {
+    if (show) {
+        IBPUmetricsWrapper.style.height = "0px";
+        IBPUmetricsWrapper.style.opacity = "0%";
+        IBPAmetricsWrapper.style.height = "0px";
+        IBPAmetricsWrapper.style.opacity = "0%";
+        TDCmetricsWrapper.style.height = "0px";
+        TDCmetricsWrapper.style.opacity = "0%";
+        DACmetricsWrapper.style.height = "0px";
+        DACmetricsWrapper.style.opacity = "0%";
+    } else {
+        IBPUmetricsWrapper.style.height = IBPUmetricsWrapper.scrollHeight;
+        IBPUmetricsWrapper.style.opacity = "100%";
+        IBPAmetricsWrapper.style.height = IBPAmetricsWrapper.scrollHeight;
+        IBPAmetricsWrapper.style.opacity = "100%";
+        TDCmetricsWrapper.style.height = TDCmetricsWrapper.scrollHeight;
+        TDCmetricsWrapper.style.opacity = "100%";
+        DACmetricsWrapper.style.height = DACmetricsWrapper.scrollHeight;
+        DACmetricsWrapper.style.opacity = "100%";
+    }
+}
+
+function showMetricsButtonPressed() {
+    if (showing) {
+        showMetricsArrow.style.transform = "rotate(90deg)";
+        showMetricsArrow.style.top = "0px"
+        showOrHideMetrics(showing)
+    } else {
+        showMetricsArrow.style.transform = "rotate(-90deg)";
+        showMetricsArrow.style.top = "-1px"
+        showOrHideMetrics(showing)
+    }
+    showing = !showing;
+}
+
+function createTransformationElement(transformationType,cellPairsList) {
+    const transformationElement = document.createElement("div");
+    const transformationColor = document.createElement("div");
+    const transformationCellPairs = document.createElement("p");
+
+    transformationElement.classList.add("transformationElement");
+    transformationColor.classList.add("transformationColor");
+    transformationCellPairs.classList.add("transformationCells");
+
+    let text = "| ";
+    for (const cellPair of cellPairsList) {
+        text += ` ${cellPair[0]} & ${cellPair[1]} |`
+    }
+
+    const colorMap = {
+        "digitSwaps1":"rgba(76, 214, 191, 1)",
+        "digitSwaps2":"rgba(77, 128, 237, 1)",
+        "digitSwaps3":"rgba(162, 112, 231, 1)",
+        "tripletSwaps":"rgba(212, 107, 132, 1)",}
+    transformationCellPairs.innerText = text;
+    transformationColor.style.backgroundColor = colorMap[transformationType];
+
+    transformationElement.onclick = () => {
+        trasnformationElementClicked(transformationElement,transformationType,cellPairsList);
+    };
+    transformationElement.appendChild(transformationColor);
+    transformationElement.appendChild(transformationCellPairs);
+    transformationsList.appendChild(transformationElement);
+}
+
+let previousTranformationElementClicked = null;
+function trasnformationElementClicked(transformationElement,transformationType,cellPairsList) {
+    if (transformationElement == previousTranformationElementClicked) {
+        previousTranformationElementClicked.classList.add("transformationElement");
+        previousTranformationElementClicked.classList.remove("transformationElementClicked");
+        previousTranformationElementClicked = null;
+        transformationSelected = null;
+        if (!visualizingAnalisis) {
+            visualizationTool.clearCanvas();
+        }
+
+        return
+    }
+    transformationElement.classList.add("transformationElementClicked");
+    transformationElement.classList.remove("transformationElement");
+    if (previousTranformationElementClicked) {
+        previousTranformationElementClicked.classList.add("transformationElement");
+        previousTranformationElementClicked.classList.remove("transformationElementClicked");
+    }
+    previousTranformationElementClicked = transformationElement;
+    
+    displayTransformation(transformationType,cellPairsList);
+    transformationSelected = {"type":transformationType,"cells":cellPairsList};
+}
+let transformationSelected = null;
+
+document.getElementById("randomGridButton").onclick = generateRandomGrid;
+function generateRandomGrid() {
+    if (visualizingAnalisis) {return}
+    const newSudokuString = sudokuLab.generateRandomGrids(1)[0];
+    sudokuLab.setNewGrid(newSudokuString);
+    renderGridDigits(newSudokuString);
+    if (sudokuLab.isGridComplete) {
+        gridCompletenessParagraph.style.color = "white";
+    }
+    if (sudokuLab.isGridValid) {
+        gridValidityParagraph.style.color = "white";
+    }
+    if (sudokuLab.isGridComplete && sudokuLab.isGridValid) {
+        analysisAlert.style.display = "none";
+    }
+}
 
 
+function applyTransformationClicked(cellPairs) {
+    sudokuLab.applyTransformation(cellPairs);
+    const sudokuString = sudokuLab.getSudokuString();
+    displayDigits(sudokuString);
+    analyzePatterns();
+    fillTransformationList();
+}
+applyTransformationButton.onclick = () => {
+    if (!transformationSelected || visualizingAnalisis) {return}
+    applyTransformationClicked(transformationSelected["cells"]);
+    transformationsSequence["sequence"].push(transformationSelected);
+}
 
+const transformationsSequence = {"sequence":[],"pointer":0};
